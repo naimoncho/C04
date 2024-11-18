0152b95a763c19e3a49e052a5edb2793069ff7f3
Naim Cheniouni Gomez <ncheniou@c2r11s2.42madrid.com>
2024-11-18 23:04:42 +0100

    yeah

---
+++ ex03/ft_atoi.c
@@ -0,0 +1,26 @@
+int ft_atoi(char *str)
+{
+    int i;
+    int sign;
+    int res;
+
+    i = 0;
+    sign = 1;
+    res = 0;
+    while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+    {
+        i ++;
+    }
+    while (str[i] == 43 || str[i] == 45)
+    {
+        if (str[i] == 45)
+            sign = sign * -1;
+            i ++;
+    }
+    while (str[i] >= '0' || str[i] <= '9')
+    {
+        res = (res * 10) + str[i] - 48;
+        i ++;
+    }
+    return (res * sign);
+}
